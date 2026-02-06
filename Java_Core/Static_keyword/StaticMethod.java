a35c2cff7cafe3ca9c1d41f2c14dcc07cb8db11d
Rahul Patil <sudhakarpatil1247@gmail.com>
2026-02-06 16:25:01 +0000

    Static Method

---
+++ Java_Core/Static_keyword/StaticMethod.java
@@ -0,0 +1,38 @@
+class Mobile {
+    String brand;
+    int price;
+    static String name;
+
+    public void show() {
+        System.out.println(brand + ":" + price + ":" + name);
+    }
+
+    public static void show1(Mobile.mob) {
+        System.out.println(brand + ":" + mob.price + ":" + name);
+    }
+
+
+}
+
+
+public class StaticMethod {
+
+    public static void main(String[] args) {
+        Mobile mob = new Mobile();
+        mob.brand = "Redmi";
+        mob.price = 29990;
+        Mobile.name = "SamrtPhone"; // refer by class name
+
+        Mobile mob1 = new Mobile();
+        mob1.brand = "Realme";
+        mob1.price = 24990;
+        Mobile.name = "SamrtPhone";  // refer by class name
+
+
+        mob.show();
+        mob1.show();
+
+        Mobile.show1(Mobile mob1);
+    }
+}
+
