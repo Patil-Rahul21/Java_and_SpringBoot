3aff16eec99555c70fb138e00555f9d447c27208
Rahul Patil <sudhakarpatil1247@gmail.com>
2026-02-06 16:25:59 +0000

    Naming Convention or Camel Casing

---
+++ NamingConv.java
@@ -0,0 +1,17 @@
+public class NamingConv {
+public static void main(String[] args) {
+    System.out.println();
+}
+    
+}
+
+// Camel Casing
+
+//class and interface = Calc, Runable
+//variable and method = marks, show()
+//constants = PIE, BRAND
+
+//showMyMarks()
+//MyData
+
+// age DATA, Human()
